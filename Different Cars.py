5a72e80e6c756f49c9db9ee671ca33f1cde9bd8e
Ark-tHe-sHaRk <arkbarot8812coder@gmail.com>
2025-03-03 17:24:35 +0000

    Add car classes with attributes and methods for brand, type, engine, speed, price, safety, and technology

---
+++ Different Cars.py
@@ -0,0 +1,82 @@
+#Class One
+class BMW:
+    def brand(self):
+        print('BMW is a German brand')
+    def type(self):
+        print('BMW is a luxury car')
+    def engine(self):
+        print('BMW cars have powerful engines')
+    def speed(self):
+        print('BMW cars are known for their speed')
+    def price(self):
+        print('BMW cars are expensive')
+    def safety(self):
+        print('BMW cars are very safe')
+    def technology(self):
+        print('BMW cars have the latest technology')
+
+#Class Two
+
+class Jaguar:
+    def brand(self):
+        print('Jaguar was a British brand now owned by Tata Motors')
+    def type(self):
+        print('Jaguar is a Luxury car')
+    def engine(self):
+        print('Jaguar cars have a powerful engine')
+    def speed(self):
+        print('Jaguar cars are know for their speed')
+    def price(self):
+        print('Jauguar cars are quite expensive')
+    def safety(self):
+        print('Jaguar cars are very safe and reliable')
+    def technology(self):
+        print('Jaguar cars havethe latest technology')
+
+#Class Three
+
+class Toyota:
+    def brand(self):
+        print('Toyota is a Japanese brand')
+    def type(self):
+        print('Toyota is a family car')
+    def engine(self):
+        print('Toyota cars have a reliable engine')
+    def speed(self):
+        print('Toyota cars are known for their mileage')
+    def price(self):
+        print('Toyota cars are affordable')
+    def safety(self):
+        print('Toyota cars are very safe')
+    def technology(self):
+        print('Toyota cars have the latest technology')
+
+#Calling the classes:
+
+B = BMW()
+B.brand()
+B.type()
+B.engine()
+B.speed()
+B.price()
+B.safety()
+B.technology()
+
+J = Jaguar()
+J.brand()
+J.type()
+J.engine()
+J.speed()
+J.price()
+J.safety()
+J.technology()
+
+T = Toyota()
+T.brand()
+T.type()
+T.engine()
+T.speed()
+T.price()
+T.safety()
+T.technology()
+
