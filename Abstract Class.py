933f53dce73373b9388243671516dd5054d91969
Ark-tHe-sHaRk <arkbarot8812coder@gmail.com>
2025-03-03 16:47:50 +0000

    Implement abstract class structure with example subclass and method demonstration

---
+++ Abstract Class.py
@@ -0,0 +1,26 @@
+#Importing Nessary modules
+from abc import ABC, abstractmethod
+
+#Creating Base Class
+class Absclass(ABC):
+    
+    #Function to print a value
+    def print(self, x):
+        print('Passed Value: ', x)
+
+    #Abstract Method
+    @abstractmethod
+    def task(self):
+        print('We are inside a Absclass Task')
+
+#Creating Subclass
+class test_class(Absclass):
+    def task(self):
+        print('We are inside a test_class Task')
+
+#Creating Object of Subclass
+test = test_class()
+test.task()
+test.print(100)
+test.task()
+
